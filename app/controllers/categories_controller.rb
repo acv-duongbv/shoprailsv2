814a2da4ed88c794e6b3750fdc3ade1fcfd0fdd0
bvduong <duongbv@atmarkcafe.org>
2018-06-12 12:06:10 +0700

    Category detail

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
   def show
+    @category = Category.find_by_slug(params[:slug])
+    @products = Product.where(category_id: @category.id)
   end
 end
