0ec172f207cd0fcd68a253f294f984b24b7cb116
bvduong <duongbv@atmarkcafe.org>
2018-06-15 10:00:19 +0700

    Pagenate

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
+gem 'will_paginate', '>= 3.0'
 gem 'stringex', '~> 2.6'
 gem 'webhoseio-ruby', '~> 1.0'
 gem 'rails', '~> 5.2.0'
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find_by_slug(params[:slug])
-    @products = Product.where(category_id: @category.id)
+    @products = Product.where(category_id: @category.id).paginate(:page => params[:page], :per_page => 8).order("price ASC")
   end
 end
