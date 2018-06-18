7be7a49b81f306a387e200e12617ac4f6c427367
bvduong <duongbv@atmarkcafe.org>
2018-06-18 17:12:01 +0700

    edit css

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,9 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  before_action do
-    redirect_to new_user_session_path unless current_user && current_user.admin?
-  end
+
   # GET /resource/sign_in
   # def new
   #   super
